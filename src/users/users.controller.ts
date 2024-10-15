fbfa04d161f41d875e3ad9ca574903e58f1d7b25
FragaGabriel23 <gabrielfraga2308@gmail.com>
2024-10-15 18:29:21 -0300

    feat: created first entity

---
+++ src/users/users.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Response } from 'express';
+import { UsersDto } from './users.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAllUsers(@Res() response: Response) {
+    return response.status(200).json(this.usersService.findAllUsers());
+  }
+
+  @Post()
+  async creatUser(@Res() response: Response, @Body() userDTO: UsersDto) {
+    const userCreated = await this.usersService.createUser(userDTO);
+    return response.status(201).json(userCreated);
+  }
+}
