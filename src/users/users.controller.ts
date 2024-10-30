75b25475ce1ce52098009f937d2bdf6b1b498baf
FragaGabriel23 <gabrielfraga2308@gmail.com>
2024-10-30 03:32:40 -0300

    feat(users): add full CRUD operations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Response } from 'express';
 import { UsersDto } from './users.dto';
@@ -8,13 +18,36 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  findAllUsers(@Res() response: Response) {
-    return response.status(200).json(this.usersService.findAllUsers());
+  async findAllUsers(@Res() response: Response) {
+    const users = await this.usersService.findAllUsers();
+    return response.status(HttpStatus.OK).json(users);
+  }
+
+  @Get(':id')
+  async findUserById(@Param('id') id: number, @Res() response: Response) {
+    const user = await this.usersService.findOneUser(id);
+    return response.status(HttpStatus.OK).json(user);
   }
 
   @Post()
-  async creatUser(@Res() response: Response, @Body() userDTO: UsersDto) {
+  async createUser(@Res() response: Response, @Body() userDTO: UsersDto) {
     const userCreated = await this.usersService.createUser(userDTO);
-    return response.status(201).json(userCreated);
+    return response.status(HttpStatus.CREATED).json(userCreated);
+  }
+
+  @Put(':id')
+  async updateUser(
+    @Param('id') id: number,
+    @Res() response: Response,
+    @Body() userDTO: UsersDto,
+  ) {
+    const updatedUser = await this.usersService.updateUser(id, userDTO);
+    return response.status(HttpStatus.OK).json(updatedUser);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: number, @Res() response: Response) {
+    await this.usersService.deleteUser(id);
+    return response.status(HttpStatus.NO_CONTENT).send();
   }
 }
